a2c6d63fe194c79426a57e07d216a5cc44d9f444
Vishnu Prabhakaran <vishnu.prabhakaran@CG-NB-566.local>
2018-07-24 17:35:56 +1000

    update

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import HomeScreen from './Components/Screens/HomeScreen';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen/>
     );
   }
 }
